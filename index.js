1c68c9c2fb1dca9a84c75f2a71973b1a8809d0c7
sara amandah <saragongora03@hotmail.com>
2024-12-11 20:56:15 -0300

    pag home parcialmente feita

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Configura o diretório de arquivos estáticos (CSS, JS, imagens)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Configura o motor de template (EJS)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Rota principal
+app.get('/', (req, res) => {
+    res.render('home'); // Renderiza o arquivo home.ejs
+});
+
+// Inicia o servidor
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
+
+
