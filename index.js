9e07937492834d5aaaddc55d94ae26102251a456
sara amandah <saragongora03@hotmail.com>
2025-02-06 08:49:12 -0300

    boas praticas de programação home

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Configura o diretório de arquivos estáticos (CSS, JS, imagens)
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
+
 
 // Configura o motor de template (EJS)
 app.set('view engine', 'ejs');
@@ -16,13 +17,17 @@ app.get('/', (req, res) => {
 });
 
 app.get('/dicas', (req, res) => {
-    res.render('dicas'); // Renderiza o arquivo oque_levar.ejs
+    res.render('dicas'); // Renderiza o arquivo dicas.ejs
 });
-
 
 // Inicia o servidor
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
 });
+
+app.use((req, res) => {
+    res.status(404).render('404'); // Certifique-se de ter um arquivo views/404.ejs
+});
+
 
 
